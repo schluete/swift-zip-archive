56c82f7e0aedc399a8eb1918661c9bee42fe0dd4
Adam Fowler <adamfowler71@gmail.com>
2025-01-30 17:13:20 +0000

    Add Foundation to Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import Foundation
 import PackageDescription
 
 let package = Package(
